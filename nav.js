54949b2026aa313d090c0754eef7bb2d92c065bc
infreeJ <kasious97@naver.com>
2025-05-01 01:37:55 +0900

    리액트 갑니다

--- nav.js
+++ nav.js
@@ -23,9 +23,15 @@ navBox.addEventListener('mouseenter', function() {
         detailList.appendChild(menuClone)
         // 세부 장소 템플릿 삽입
 
+        setTimeout(function() {
+            detailList.style.maxHeight = '30vh'
+        }, 10)
+        // 부드럽게 나오는 애니메이션
+        
         i++;
     }
 })
+
 
 // 네비게이션 세부목록 축소
 
@@ -36,7 +42,14 @@ navBox.addEventListener("mouseleave", function() {
         // 뒷배경 제거
 
         const detailList = document.querySelector(".detail-list");
-        detailList.remove();
+        setTimeout(function() {
+            detailList.style.maxHeight = '0'
+        }, 10)
+        // 세부목록 축소
+
+        setTimeout(function() {
+            detailList.remove();
+        }, 400)
         // 세부목록 제거
 
         i = i - 1;
